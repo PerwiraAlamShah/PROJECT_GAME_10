e5f0696039e0b872bacfaa8a0d1f0a1459bdb9d4
PerwiraAlamShah <perwiraalamshah@gmail.com>
2022-08-31 11:28:27 +0700

    Animation, isGrounded, itemPickUp, textScore (Orange)

--- PROJECT_GAME_10/Assets/Pixel Adventure 1/Assets/Main Characters/Virtual Guy/Scripts/PlayerController.cs
+++ PROJECT_GAME_10/Assets/Pixel Adventure 1/Assets/Main Characters/Virtual Guy/Scripts/PlayerController.cs
@@ -7,10 +7,17 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
     private Animator anim;
+
+    [SerializeField] private LayerMask jumpalbeGround;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float Jump = 14f;
     private float dirX;
+
+    // Movement State
+    private enum MovementState { idle, run, fall, jump };
+    private MovementState state = MovementState.idle;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +25,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -34,7 +42,7 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, Jump);
         }
@@ -44,20 +52,41 @@ public class PlayerController : MonoBehaviour
     #region UpdateAnimation
     void UpdateAnimation()
     {
+        MovementState state;
+
         if (dirX > 0f)
         {
             sprite.flipX = false;
-            anim.SetBool("Run", true);
+            state = MovementState.run;
         }
         else if (dirX < 0f)
         {
             sprite.flipX = true;
-            anim.SetBool("Run", true);
+            state = MovementState.run;
         }
         else
         {
-            anim.SetBool("Run", false);
+            state = MovementState.idle;
         }
+
+        if (rb.velocity.y > .1f)
+        {
+            state = MovementState.jump;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.fall;
+        }
+
+        anim.SetInteger("state", (int)state);
     }
+    #endregion
+
+    #region IsGround
+    private bool isGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpalbeGround);
+    }
+
     #endregion
 }
